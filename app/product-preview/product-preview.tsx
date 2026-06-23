8dd3d52db9cdc7c887167ca018b2c28108f1bcd8
MOG\itspa <itspandorux@gmail.com>
2026-06-23 16:22:40 +1000

    Fixed Layout Issues w/ Product Card Info

--- app/product-preview/product-preview.tsx
+++ app/product-preview/product-preview.tsx
@@ -3,20 +3,46 @@ import "./product-preview.css";
 export function ProductPreview() {
   return (
     <>
-      <div className="container productCard" style={{ height: "50vh" }}>
+      <div
+        className="container productCard"
+        style={{
+          height: "50vh",
+          borderRadius: "12.5px",
+          overflow: "hidden",
+          padding: "0px",
+        }}
+      >
         <div className="row" style={{ height: "100%" }}>
           <div className="col-6" style={{ height: "100%", padding: "0px" }}>
             <img
-              style={{ height: "100%" }}
+              style={{
+                height: "100%",
+                width: "100%",
+                objectFit: "cover",
+              }}
               src="image-product-desktop.jpg"
             ></img>
           </div>
-          <div className="col" style={{ padding: "12px" }}>
-            <div className="row">PERFUME</div>
-            <div className="row">Gabrielle Essence Eau De Parfum</div>
+          <div
+            className="col"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+              alignItems: "flex-start",
+            }}
+          >
             <div className="row">
-              A floral, solar and voluptuous interpretation composed by Olivier
-              Polge, Preformer-Creator for the House of CHANEL
+              <p>PERFUME</p>
+            </div>
+            <div className="row">
+              <p>Gabrielle Essence Eau De Parfum</p>
+            </div>
+            <div className="row">
+              <p>
+                A floral, solar and voluptuous interpretation composed by
+                Olivier Polge, Preformer-Creator for the House of CHANEL
+              </p>
             </div>
             <div className="row">
               <div className="col">$149.99</div>
